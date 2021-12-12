bb0dde9ee05e2b3efb9a9c0d2c87c6fa66b83eb3
liertsa <89617693+liertsa@users.noreply.github.com>
2021-12-13 00:57:46 +0800

    first commit

---
+++ LA CERVEZA ECOMM/lc.js
@@ -0,0 +1,95 @@
+
+function myFunction() {
+    alert("");
+  }
+  if (confirm("Are you 18 and above?")) {
+      txt = "You pressed OK!";
+  } else {
+      txt = "You pressed Cancel!";
+ }
+
+
+ window.onload = function() {
+    //get the canvas and conext and store in vars
+    var canvas = document.getElementById("sky");
+    var ctx = canvas.getContext("2d");
+    
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+    canvas.width = W;
+    canvas.height = H;
+    
+    var mf = 100;
+    var flakes = [];
+    
+    for(var i=0; i<mf; i++) {
+        flakes.push({
+            x: Math.random()*W,
+            y: Math.random()*H,
+            r: Math.random()*5+2,
+            d: Math.random()+1
+        });
+    }
+    
+    function drawFlakes() {
+        ctx.clearRect(0, 0, W, H);
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        for(var i=0; i<mf; i++) {
+            var f = flakes[i];
+            ctx.moveTo(f.x, f.y);
+            ctx.arc(f.x, f.y, f.r, 0, Math.PI*2, true);
+        }
+        ctx.fill();
+        moveFlakes();
+    }
+    
+    var angle = 0; 
+    
+    function moveFlakes() {
+        angle += 0.01;
+        for(var i=0; i<mf; i++) {
+            var f = flakes[i];
+            
+            f.y += Math.pow(f.d, 2) * 1;
+            f.x += Math.sin(angle) * 2;
+            
+            if(f.y > H) {
+                flakes[i] = {
+                    x: Math.random()*W,
+                    y: 0,
+                    r: f.r, 
+                    d: f.d
+                }
+            }
+        }
+                
+    }
+    
+    setInterval(drawFlakes, 25);
+}
+//  let search = document.querySelector('.search-box');
+
+// document.querySelector('#search-icon').onclick = () => {
+//     search.classList.toggle('active');
+//     navbar.classList.remove('active');
+// }
+
+// let navbar = document.querySelector('.navbar');
+
+// document.querySelector('#menu-icon').onclick = () => {
+//     navbar.classList.toggle('active');
+//         search.classList.remove('active');
+// }
+
+// window.onscroll = () => {
+//     navbar.classList.remove('active');
+//         search.classList.remove('active');
+// }
+// let header = document.querySelector('header');
+
+// window.addEventListener('scroll', () => {
+//     header.classList.toggle('shadow', window.scrollY > 0)
+// });
+
+
